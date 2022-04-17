57fb564f56ae00811e110c25e962e048cfe21894
samraasif834 <samraasif834@gmail.com>
2022-04-17 23:48:02 +0500

    update

--- lib/onboardingscree.dart
+++ lib/onboardingscree.dart
@@ -1,37 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class Onboarding extends StatefulWidget {
-  const Onboarding({ Key? key }) : super(key: key);
+  const Onboarding({Key? key}) : super(key: key);
 
   @override
   State<Onboarding> createState() => _OnboardingState();
 }
 
 class _OnboardingState extends State<Onboarding> {
+  final controller = PageController();
   @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        children: [
-          Container(
-            color: Colors.red,
-            child: Center(
-              child: Text('page 1'),
+      body: Container(
+        padding: const EdgeInsets.only(bottom: 80),
+        child: PageView(
+          controller: controller,
+          children: [
+            Container(
+              color: Colors.red,
+              child: Center(
+                child: Text('page 1'),
+              ),
             ),
-          ),
-          Container(
-             color: Colors.indigo,
-            child: Center(
-              child: Text('page 2'),
-          ),
-          ),
-          Container(
-             color: Colors.green,
-            child: Center(
-              child: Text('page 3'),
-          ),
-          )
-        ],
+            Container(
+              color: Colors.indigo,
+              child: Center(
+                child: Text('page 2'),
+              ),
+            ),
+            Container(
+              color: Colors.green,
+              child: Center(
+                child: Text('page 3'),
+              ),
+            )
+          ],
+        ),
+      ),
+      bottomSheet: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        height: 80,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            TextButton(onPressed: () {}, child: Text('Skip')),
+            Center(
+              child:SmoothPageIndicator(
+                controller:controller,
+                count:3,
+              ),
+            ),
+            TextButton(onPressed: () {}, child: Text('next'))
+          ],
+        ),
       ),
     );
   }
