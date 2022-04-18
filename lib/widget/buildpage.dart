3e4641410a3aeed1ab2f372fc11ae099d47cb0f1
samraasif834 <samraasif834@gmail.com>
2022-04-18 18:30:02 +0500

    update

--- lib/widget/buildpage.dart
+++ lib/widget/buildpage.dart
@@ -9,6 +9,25 @@ class BuilPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: color,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(urlimage,
+          fit: BoxFit.cover,
+          width: double.infinity,
+          ),
+          const SizedBox(height:64),
+          Text(title,style: TextStyle(
+            color: Colors.teal.shade700,
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+            
+          ),),
+          
+        ],
+      ),
+    );
   }
 }
